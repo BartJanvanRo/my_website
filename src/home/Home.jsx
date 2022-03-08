3c3f0ef86a739ef3c59e51608be9a7c4821e583e
BartJanvanRo <hjavanrossum@gmail.com>
2022-03-08 22:28:43 +0100

    8 maart

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,9 @@
-import portrait from '../img/portrait.png';
 import { useState, useEffect } from 'react';
 import arrow from '../img/button/arrowBttnVerti.svg'
 
 import { db } from '../firebase'
-import { collection, doc, getDocs } from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
+import { Link, useLocation } from "react-router-dom";
 
 function Home() {
     const [content, setContent] = useState([]);
@@ -17,47 +17,67 @@ function Home() {
             setContent(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         }
         getContent();
-        console.log(getContent)
+
+
 
     }, [])
 
 
 
+    const idCheck = content.map(e => {
+        return e.id
+    })
+    //console.log(document.title)
+    //console.log(idCheck)
+    //we zoeken in een array dezelfde naam en dan de onderliggende data ophalen, vergelijking id  
+    document.title = "homepage"
+
     return (
-
-
         <div className="generalContainerBody">
-            <div className="container">
-                <div className="top_title">
+            {""}
+            {content.map((icontent) => {
+                if (icontent.id == document.title) {
+                    console.log(icontent)
+                    return (
+                        <div className="container">
+                            <div className="top_title">
+                                {icontent.title}
 
-                </div>
-                <div className="flexContainer">
-                    <div className="bottomLeft">
-                        <img src={portrait} alt="My Drawing" />
-                    </div>
-                    <div className="bottomRight">
-                        <h2>Bart-Jan?</h2>
-                        <br />
-                        Ik ben een zoekende ziel die zijn creativiteit op verschillende manieren uitdrukt, die graag  verschillende technieken leert kennen en toepassen. Ondanks het creative werk ik toch structureel en analytisch. Energie krijg  ik van input om me heen, bijv. interviews, projectmatige samenwerking, overleg met opdrachtgevers, collega’s en users. <br /> UI/UX geeft me een kans om creatief te zijn op een toegepaste manier, zowel een functionele toepassing te creëren alsmede een visueel pareltje om naar te kijken.
-                        <br /><br />
-                        Naast het werken met computers ben ik graag bezig met mijn handen, ik schilderen en teken graag. Voor sociale interactie kan ik me helemaal uitleven op het badmintonveld en achteraf gezellig napraten in de kantine.
+                            </div>
+                            <div className="flexContainer">
+                                <div className="bottomLeft">
+                                    <img src={icontent.image1} alt="My Drawing" />
+                                </div>
+                                <div className="bottomRight">
+                                    <h2>{icontent.subTitle}</h2>
+                                    <br />
+                                    {icontent.text}
 
-                    </div>
-                    {/*     <div className="rightBttm">
+                                </div>
+                                {/*     <div className="rightBttm">
 
                         UI <br />
                         1/2
                         <img src={arrow} alt="arrow" />
     </div> */}
-                </div>
-                <div className="vertiBttm">
-                    UI/<br />
-                    UX <br />
-                    <img src={arrow} alt="arrow" />
-                </div>
-            </div>
-        </div>
+                            </div>
+
+                            <div className="vertiBttm">
+                                <Link to="/UI">
+                                    UI/<br />
+                                    UX <br />
+                                    <img src={arrow} alt="arrow" />
+                                </Link>
+                            </div>
+
+                        </div>
+                    );
+                }
+            })}
+        </div >
     )
+
+
 }
 
 export default Home
